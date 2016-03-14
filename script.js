f2cd6c7caf4139aaaae4c5525cc474c115ed9eb1
Aleksa Rakic <rakicaleksa@ymail.com>
2016-03-14 16:11:36 +0100

    bs classes for mobile

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
 			success: function(response){
 				var channelName = url.substr(url.lastIndexOf('/')+1);
 				if(response.stream === null){
-					$('.streamers').append('<div class="streamer off"><div class="row"><div class="col-md-2 pic-cont"><div class="pic" style="background: url(images/nosignal.png); background-size: cover;"></div></div><div class="col-md-7 channel-name"><a class="streamer-link" href="http://twitch.tv/'+ channelName +'">'+channelName+'</a></div><div class="col-md-3 game">Offline</div></div></div>');
+					$('.streamers').append('<div class="streamer off"><div class="row"><div class="col-sm-2 pic-cont"><div class="pic" style="background: url(images/nosignal.png); background-size: cover;"></div></div><div class="col-sm-7 channel-name"><a class="streamer-link" href="http://twitch.tv/'+ channelName +'">'+channelName+'</a></div><div class="col-sm-3 game">Offline</div></div></div>');
 				}else if(response.status === 422){
-					$('.streamers').append('<div class="streamer off"><div class="row"><div class="col-md-2 pic-cont"><div class="pic" style="background: url(images/nosignal.png); background-size: cover;"></div></div><div class="col-md-7 channel-name"><a class="streamer-link" href="http://twitch.tv/'+ channelName +'">'+channelName+'</a></div><div class="col-md-3 game">Dead channel</div></div></div>');
+					$('.streamers').append('<div class="streamer off"><div class="row"><div class="col-sm-2 pic-cont"><div class="pic" style="background: url(images/nosignal.png); background-size: cover;"></div></div><div class="col-sm-7 channel-name"><a class="streamer-link" href="http://twitch.tv/'+ channelName +'">'+channelName+'</a></div><div class="col-sm-3 game">Dead channel</div></div></div>');
 				}else{
-					$('.streamers').prepend('<div class="streamer onl"><div class="row"><div class="col-md-2 pic-cont"><div class="pic" style="background: url('+response.stream.channel.logo+'); background-size: cover;"></div></div><div class="col-md-7 channel-name"><a class="streamer-link" href="http://twitch.tv/'+ response.stream.channel.name +'">'+response.stream.channel.name+'</a></div><div class="col-md-3 game">Playing: '+ response.stream.game+'<p id="viewers">Viewers: '+response.stream.viewers +'</p></div></div>');
+					$('.streamers').prepend('<div class="streamer onl"><div class="row"><div class="col-sm-2 pic-cont"><div class="pic" style="background: url('+response.stream.channel.logo+'); background-size: cover;"></div></div><div class="col-sm-7 channel-name"><a class="streamer-link" href="http://twitch.tv/'+ response.stream.channel.name +'">'+response.stream.channel.name+'</a></div><div class="col-sm-3 game">Playing: '+ response.stream.game+'<p id="viewers">Viewers: '+response.stream.viewers +'</p></div></div>');
 				}
 			}
 		});
